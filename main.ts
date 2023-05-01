7cef46d688766d4d20fe17d8448180df0bab9e85
andrea pagliari <andrea.pagliari12@gmail.com>
2023-05-01 09:13:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,9 +26,11 @@ let distance_RIGHT = 0
 let distance_LEFT = 0
 let distance = 0
 let speedNormal = 0
-basic.showIcon(IconNames.Surprised)
+basic.showIcon(IconNames.EigthNote)
 speedNormal = 30
 let distanceLimit = 40
+let LEFTTurnAngle = 135
+let RIGHTTurnAngle = 45
 basic.forever(function () {
     distance = mecanumRobot.ultra()
     if (distance < distanceLimit) {
@@ -36,22 +38,22 @@ basic.forever(function () {
         mecanumRobot.state(MotorState.stop)
         basic.pause(500)
         // turn head LEFT
-        mecanumRobot.setServo(180)
+        mecanumRobot.setServo(LEFTTurnAngle)
         basic.pause(500)
         distance_LEFT = mecanumRobot.ultra()
         basic.pause(500)
-        mecanumRobot.setServo(0)
+        mecanumRobot.setServo(RIGHTTurnAngle)
         basic.pause(500)
         distance_RIGHT = mecanumRobot.ultra()
         basic.pause(500)
         if (distance_LEFT > distance_RIGHT) {
             car_left()
             mecanumRobot.setServo(90)
-            basic.pause(300)
+            basic.pause(1000)
         } else {
             car_right()
             mecanumRobot.setServo(90)
-            basic.pause(300)
+            basic.pause(1000)
         }
     } else {
         car_forward()
